b3ff80d3bc417e2602091d3abfe208f0ff026902
TheTomatoXD <97130154+TheTomatoXD@users.noreply.github.com>
2023-09-18 21:09:18 +0200

    Initial commit

---
+++ src/main/java/net/tomatoxd/clientqol/ClientQoLClient.java
@@ -0,0 +1,10 @@
+package net.tomatoxd.clientqol;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientQoLClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tomatoxd/clientqol/ClientQoLDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tomatoxd.clientqol;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ClientQoLDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
